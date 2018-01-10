ab36c3341b1386fa3dcbcd0aa953687f921b04ea
Jack <flavus@gmail.com>
2018-01-10 17:23:56 -0500

    Update
    Removed unnecessary files and folders. Updated index to current
    version.
    Still need to figure out why accents aren’t showing up. Also I’d like
    to add a Stat of the day.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>   
-  <meta charset="UTF-8"> 
+  <meta charset="windows-1252"> 
   <title>TMBU</title>
   <link rel="stylesheet" href="css/carousel.css">
   <!--<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/overpass" rel="stylesheet" type="text/css"/> -->
@@ -8,18 +8,6 @@
   <link href='http://fonts.googleapis.com/css?family=PT+Serif|Playfair+Display&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
   
- <!-- Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-48451366-2', 'auto');
-  ga('send', 'pageview');
-
-</script>
-<!-- End Google Analytics -->
  
   
 
@@ -43,13 +31,13 @@
       <h1>The MoneyBall Union</h1>
       <nav>
         <ul>
-          <li><a href="http://tmbu.invisionzone.com/" onclick="ga('send', 'event', 'link', 'click-Forum', 'http://tmbu.invisionzone.com/')">Forum</a></li>
-          <li><a href="http://themoneyballunion.com/game/lgfile/tmbu.tar.gz" onclick="ga('send', 'event', 'link', 'click-LeagueFile', 'http://themoneyballunion.com/game/lgfile/tmbu.tar.gz')">League File</a></li>
-          <li><a href="http://themoneyballunion.com/game/StatsLab/login.php" onclick="ga('send', 'event', 'link', 'click-StatsLab', 'http://themoneyballunion.com/game/StatsLab/login.php')">StatsLab</a></li>
-          <li><a href="http://themoneyballunion.com/game/lgreports/" onclick="ga('send', 'event', 'link', 'click-Reports', 'http://themoneyballunion.com/game/lgreports/')">Reports</a></li>
-          <li><a href="http://themoneyballunion.com/game/lgreports/history/index.html" onclick="ga('send', 'event', 'link', 'click-History', 'http://themoneyballunion.com/game/lgreports/history/index.html')">History</a></li>
-          <li><a href="http://themoneyballunion.com/game/exporttracker4/" onclick="ga('send', 'event', 'link', 'click-ExportTracker', 'http://themoneyballunion.com/game/exporttracker4/')">Export Tracker</a></li>
-          <li><a href="http://themoneyballunion.com/game/lgreports/online_league_status.html" onclick="ga('send', 'event', 'link', 'click-ExportStatus', 'http://themoneyballunion.com/game/lgreports/online_league_status.html')">Export Status</a></li>
+          <li><a href="http://tmbu.invisionzone.com/">Forum</a></li>
+          <li><a href="http://tmbu.sportsrealm.net/lgexports/tmbu.lg.zip">League File</a></li>
+          <li><a href="http://tmbu.sportsrealm.net/statslab/login.php">StatsLab</a></li>
+          <li><a href="http://tmbu.sportsrealm.net/lgreports/index.html">Reports</a></li>
+          <li><a href="http://tmbu.sportsrealm.net/lgreports/history/index.html">History</a></li>
+          <li><a href="http://tmbu.sportsrealm.net/lgexports/exports.php">Export Tracker</a></li>
+          <li><a href="http://tmbu.sportsrealm.net/lgreports/online_league_status.html">Export Status</a></li>
         </ul>
       </nav>
       <div>
@@ -62,38 +50,38 @@
       <h3>HL</h3>
       <div>
         <?php 
-          $html = getHTML('http://themoneyballunion.com/game/StatsLab/widget.php?show=standings&division=2&sub_league=1', 10);
+          $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=standings&division=2&sub_league=1', 10);
           echo $html;
         ?>
       </div>
       <div>
         <?php 
-          $html = getHTML('http://themoneyballunion.com/game/StatsLab/widget.php?show=standings&division=1&sub_league=1', 10);
+          $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=standings&division=1&sub_league=1', 10);
           echo $html;
         ?>
       </div>
       <div>
         <?php 
-          $html = getHTML('http://themoneyballunion.com/game/StatsLab/widget.php?show=standings&division=0&sub_league=1', 10);
+          $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=standings&division=0&sub_league=1', 10);
           echo $html;
         ?>
       </div>
       <h3>SL</h3>
       <div>
         <?php 
-          $html = getHTML('http://themoneyballunion.com/game/StatsLab/widget.php?show=standings&division=2&sub_league=0', 10);
+          $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=standings&division=2&sub_league=0', 10);
           echo $html;
         ?>
       </div>
       <div>
         <?php 
-          $html = getHTML('http://themoneyballunion.com/game/StatsLab/widget.php?show=standings&division=1&sub_league=0', 10);
+          $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=standings&division=1&sub_league=0', 10);
           echo $html;
         ?>
       </div>
       <div>
         <?php 
-          $html = getHTML('http://themoneyballunion.com/game/StatsLab/widget.php?show=standings&division=0&sub_league=0', 10);
+          $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=standings&division=0&sub_league=0', 10);
           echo $html;
         ?>
       </div>
@@ -105,13 +93,13 @@
         <div class='panel' >
           <h3>SL Player of the Week</h3>
             <?php
-              $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?show=potw&league_id=100&sub_league=0', 10);
+              $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?show=potw&league_id=100&sub_league=0', 10);
               $html = utf8_encode($html);
               echo $html;
             ?>
           <h3>HL Player of the Week</h3>
             <?php
-              $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?show=potw&league_id=100&sub_league=1', 10);
+              $html = getHTML('http://tmbu.sportsrealm.net/statslab/widget.php?show=potw&league_id=100&sub_league=1', 10);
               $html = utf8_encode($html);
               echo $html;
             ?>    
@@ -119,14 +107,14 @@
         <div class='panel'>
           <h3>WAR leaders</h3>
             <?php
-              $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?stat=war&topX=10', 10);
+              $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?stat=war&topX=10', 10);
               $html = utf8_encode($html);
               echo $html;
               
             ?>
             <h3>pWAR leaders</h3>
             <?php
-              $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?stat=pwar&topX=10', 10);
+              $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?stat=pwar&topX=10', 10);
               $html = utf8_encode($html);
               echo $html;
               
@@ -135,14 +123,14 @@
         <div class='panel'>
           <h3>HR leaders</h3>
           <?php
-            $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?stat=hr&topX=10', 10);
+            $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?stat=hr&topX=10', 10);
             $html = utf8_encode($html);
             echo $html;
             
           ?>
           <h3>Hit leaders</h3>
           <?php
-            $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?stat=h&topX=10', 10);
+            $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?stat=h&topX=10', 10);
             $html = utf8_encode($html);
             echo $html;
             
@@ -151,14 +139,14 @@
         <div class='panel'>
           <h3>K leaders</h3>
           <?php
-            $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?stat=pk&topX=10', 10);
+            $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?stat=pk&topX=10', 10);
             $html = utf8_encode($html);
             echo $html;
             
           ?>
           <h3>QS leaders</h3>
           <?php
-            $html = getHTML('http://themoneyballunion.com/game/StatsLab//widget.php?stat=qs&topX=10', 10);
+            $html = getHTML('http://tmbu.sportsrealm.net/statslab//widget.php?stat=qs&topX=10', 10);
             $html = utf8_encode($html);
             echo $html;
             
@@ -171,8 +159,8 @@
       <div class="articles">
       <?php 
         include('simple_html_dom.php');
-        //set up curl
-        $ch = curl_init("http://www.themoneyballunion.com/game/lgreports//leagues/league_100_home.html");
+        //set up curl 
+        $ch = curl_init("http://tmbu.sportsrealm.net/lgreports//leagues/league_100_home.html");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         $curl_html = curl_exec($ch); //use curl to get data from example.com
@@ -186,7 +174,7 @@
 
 
         //reformat links
-        $urlAdd = "http://www.themoneyballunion.com/game/lgreports/";
+        $urlAdd = "http://tmbu.sportsrealm.net/lgreports/";
         $stringToRemove = "..";
         $re = "/(\.){2}/"; 
         $articles = preg_replace($re, $urlAdd, $articles);
@@ -204,7 +192,7 @@
     
     <footer>
       <ul>
-        <li>&copy; TMBU 2015</li>
+        <li>&copy; TMBU 2018</li>
         <li><a href="http://www.ootpdevelopments.com/board">OOTP Boards</a></li>
         <li>OOTP 16</li>
       </ul>
